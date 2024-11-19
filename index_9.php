9724e0e9d82a4bc17ab91cc3409aabba28c12087
NEON <mgutie33@hotmail.com>
2024-11-19 10:09:45 -0500

    first commit

---
+++ index_9.php
@@ -0,0 +1,100 @@
+
+<?php
+/*
+include "config.php";
+include "utils.php";
+
+$dbConn =  connect($db);*/
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+    $vbvb = "[{'id':'1','title':'5','status':'draft','content':'1','user_id':'2'},{'id':'2','title':'52q','status':'draft','content':'1qq','user_id':'2'},{'id':'3','title':'m2q','status':'draft','content':'zzq','user_id':'2'}]";    
+    echo json_encode( $vbvb );
+    exit();
+}
+
+/*
+  listar todos los posts o solo uno
+ */
+/*if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+    if (isset($_GET['id']))
+    {
+      //Mostrar un post
+      $sql = $dbConn->prepare("SELECT * FROM posts where id=:id");
+      $sql->bindValue(':id', $_GET['id']);
+      $sql->execute();
+      header("HTTP/1.1 200 OK");
+      echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
+      exit();
+	  }
+    else {
+      //Mostrar lista de post
+      $sql = $dbConn->prepare("SELECT * FROM posts");
+      $sql->execute();
+      $sql->setFetchMode(PDO::FETCH_ASSOC);
+      header("HTTP/1.1 200 OK");
+      echo json_encode( $sql->fetchAll() );
+      exit();
+	  }
+}*/
+
+// Crear un nuevo post
+/*if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $input = $_POST;
+    $sql = "INSERT INTO posts
+          (title, status, content, user_id)
+          VALUES
+          (:title, :status, :content, :user_id)";
+    $statement = $dbConn->prepare($sql);
+    bindAllValues($statement, $input);
+    $statement->execute();
+    $postId = $dbConn->lastInsertId();
+    if($postId)
+    {
+      $input['id'] = $postId;
+      header("HTTP/1.1 200 OK");
+      echo json_encode($input);
+      exit();
+	 }
+}*/
+
+//Borrar
+/*if ($_SERVER['REQUEST_METHOD'] == 'DELETE')
+{
+	$id = $_GET['id'];
+  $statement = $dbConn->prepare("DELETE FROM posts where id=:id");
+  $statement->bindValue(':id', $id);
+  $statement->execute();
+	header("HTTP/1.1 200 OK");
+	exit();
+}*/
+
+//Actualizar
+/*if ($_SERVER['REQUEST_METHOD'] == 'PUT')
+{
+    $input = $_GET;
+    $postId = $input['id'];
+    $fields = getParams($input);
+
+    $sql = "
+          UPDATE posts
+          SET $fields
+          WHERE id='$postId'
+           ";
+
+    $statement = $dbConn->prepare($sql);
+    bindAllValues($statement, $input);
+
+    $statement->execute();
+    header("HTTP/1.1 200 OK");
+    exit();
+}*/
+
+
+//En caso de que ninguna de las opciones anteriores se haya ejecutado
+header("HTTP/1.1 400 Bad Request");
+
+?>
